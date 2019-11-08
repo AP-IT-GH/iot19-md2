c0b66a1a0eb088fb8d58ca66009e611ec7b1ef43
imad <eaimad44@gmail.com>
2019-11-08 11:49:53 +0100

    angular first commit

--- src/Angular/medicaldelivery/src/app/app.module.ts
+++ src/Angular/medicaldelivery/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule, MatCardModule } from "@angular/material"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
