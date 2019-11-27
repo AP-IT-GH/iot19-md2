e95e42358e7b0a42f82436a7880f9497047aa9b2
Unknown <cristian.baba09@yahoo.com>
2019-11-27 21:40:55 +0100

    Map working with arr of points

--- src/Angular/medicaldelivery/src/app/app.module.ts
+++ src/Angular/medicaldelivery/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LeafletModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
