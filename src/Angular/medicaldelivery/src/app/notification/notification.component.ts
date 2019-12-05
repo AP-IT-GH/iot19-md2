163314500d446d25b27004d9c3274d2d430d6b20
Unknown <cristian.baba09@yahoo.com>
2019-12-05 16:36:05 +0100

    Dialog working

---
+++ src/Angular/medicaldelivery/src/app/notification/notification.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { VERSION, MatDialogRef, MatDialog, MatSnackBar, MAT_DIALOG_DATA } from '@angular/material';
+import { DialogComponent } from '../dialog/dialog.component';
+
+@Component({
+  selector: 'app-notification',
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.css']
+})
+export class NotificationComponent implements OnInit {
+
+
+
+  constructor(public dialog: MatDialog){}
+    
+
+  
+  ngOnInit() {
+
+    }
+
+    showDialog(){
+      const dialogRef = this.dialog.open(DialogComponent, {
+        width: '450px',
+        height: '200px'
+      }); 
+      setTimeout(() => {
+        dialogRef.close();
+      }, 10000);
+    }
+  }
+
+
